620c04836f1814a734a96ada85386e4a99135279
Simen Spets Storhov <simen.s.storhov@gmail.com>
2025-05-13 13:51:13 +0200

    fix: fjern ubrukt stripePromise-variabel (lint-feil)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { loadStripe } from "@stripe/stripe-js";
 import FeatureSection from "@/components/FeatureSection";
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 const EFFECTS = [
   "confident shoppers",
